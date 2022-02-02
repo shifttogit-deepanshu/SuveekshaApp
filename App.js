af837b9f2f146ae64d4a91c28e1be8e6aa811492
shifttogit-deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-02-02 13:13:42 +0530

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView  } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+         <WebView 
+      style={styles.container}
+      source={{ uri: 'https://lms.suveeksha.com/' }}
+    />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width:"100%",
+    height:"100%"
   },
 });
